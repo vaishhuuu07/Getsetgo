b71332b50e80491fcc0fe442b0ed4fa25c72e562
vaishhuuu07 <119472392+vaishhuuu07@users.noreply.github.com>
2023-08-30 13:46:53 +0530

    db conn

---
+++ Backend/mysqlconfig.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: 'your_database_host',
+  user: 'your_database_user',
+  password: 'your_database_password',
+  database: 'your_database_name'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+  } else {
+    console.log('Connected to MySQL database');
+  }
+});
